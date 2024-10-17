452420fa6594a1d3909308987af0a99108b93c26
AlexGomes7 <alexandre.gomes@checkmarx.com>
2024-10-17 14:54:16 +0100

    no custom lb

--- main.tf
+++ main.tf
@@ -41,49 +41,15 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier  = module.blog_vpc.public_subnets
-
-  traffic_source_attachments = module.blog_alb.target_group_attachments
-  security_groups      = [module.blog_sg.security_group_id]
+  vpc_zone_identifier        = module.blog_vpc.public_subnets
+  security_groups            = [module.blog_sg.security_group_id]
   
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
 }
 
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
 
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "Dev"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
